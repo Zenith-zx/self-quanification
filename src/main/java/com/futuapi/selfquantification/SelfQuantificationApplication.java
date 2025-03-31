25bebe2497421130ec6a171d356d5ba215af718a
zhouxin66 <673281075@qq.com>
2025-03-31 14:58:05 +0800

    add file

---
+++ src/main/java/com/futuapi/selfquantification/SelfQuantificationApplication.java
@@ -0,0 +1,13 @@
+package com.futuapi.selfquantification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfQuantificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfQuantificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/futuapi/selfquantification/SelfQuantificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futuapi.selfquantification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfQuantificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
